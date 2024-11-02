1adbf170430d8780df5b906eb42954d08c8664e7
Skuolfi <github.bulginess030@passmail.net>
2024-11-02 11:57:40 +0000

    Add files via upload
    Initial version

---
+++ utils.cpp
@@ -0,0 +1,21 @@
+//
+// This work is licensed under
+// Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International.
+// By Tapio & Visa Ryhänen
+// To view a copy of this license,
+// please visit https://creativecommons.org/licenses/by-nc-sa/4.0/
+// Dedicated to the memory of Kristian "Rico" Guttorm.
+// To compile, run:
+// g++ -o oza satnegirji.cpp utils.cpp -lsqlite3
+//
+
+#include "utils.h"
+#include <cstdlib>
+
+void Utils::clearScreen() {
+	#ifdef _WIN32
+		system("cls");
+	#else
+		system("clear");
+	#endif
+}
