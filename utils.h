254e6b076ac7e96948512588ffb722e7fca2d4cc
Skuolfi <github.bulginess030@passmail.net>
2024-11-02 11:58:10 +0000

    Add files via upload
    Initial version

---
+++ utils.h
@@ -0,0 +1,20 @@
+//
+// This work is licensed under
+// Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International.
+// By Tapio & Visa Ryhänen
+// To view a copy of this license,
+// please visit https://creativecommons.org/licenses/by-nc-sa/4.0/
+// Dedicated to the memory of Kristian "Rico" Guttorm.
+// To compile, run:
+// g++ -o oza satnegirji.cpp utils.cpp -lsqlite3
+//
+
+#ifndef UTILS_H
+#define UTILS_H
+
+class Utils {
+	public:
+		void clearScreen();
+};
+
+#endif
